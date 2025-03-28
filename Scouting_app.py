c4ba54bf742aeb5118982eaf01d68ac32c59b82f
RemiBounoua7 <remi.bounoua@telecom-sudparis.eu>
2025-03-28 16:35:04 +0100

    debug comparison

--- Scouting_app.py
+++ Scouting_app.py
@@ -175,7 +175,6 @@ def calculate_hexbin_stats(df, x_col='LOC_X', y_col='LOC_Y', shot_col='SHOT_MADE
     out=np.zeros_like(total_successes, dtype=float), 
     where=total_attempts > 0
     )
-    st.write("FG% : ",fg_percentages)
     # Normalize shot volumes (for this dataset)
     norm_volumes = volumes / volumes.max() if volumes.max() > 0 else volumes
 
@@ -186,7 +185,7 @@ def calculate_hexbin_stats(df, x_col='LOC_X', y_col='LOC_Y', shot_col='SHOT_MADE
         'norm_volumes': norm_volumes,
         'fg_percentages': fg_percentages
     }
-
+    st.write("dict : ",stats_dict)
     return stats_dict
 
 def compare_player_to_global(df, player_name, x_col='LOC_X', y_col='LOC_Y', shot_col='SHOT_MADE_FLAG',
