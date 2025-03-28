8b06fdaf6ee38df27d4e0a03f3a45c60e75c75f1
RemiBounoua7 <remi.bounoua@telecom-sudparis.eu>
2025-03-28 13:13:56 +0100

    debug hex

--- Scouting_app.py
+++ Scouting_app.py
@@ -339,11 +339,11 @@ def get_expected_pts(comparison,shotchart,season_df,game_df):
 
         value=int(value)
         hex = get_hex(comparison,x,y)
+        st.write(hex)
         x_pts += value*comparison[hex]['player_fg']
 
 
     x_pts += game_df['FTA'].values[0] * season_df['FT_PCT'].mean()
-    st.write(x_pts)
     return round(x_pts,1)
 
 def get_player_season_averages(season_df):
