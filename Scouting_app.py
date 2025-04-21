ec6a099838842fbe18156ab2a063b06ee2e2c1d0
RemiBounoua7 <remi.bounoua@telecom-sudparis.eu>
2025-04-21 11:31:26 +0200

    real_app outline

--- Scouting_app.py
+++ Scouting_app.py
@@ -488,10 +488,10 @@ game_labels = ["MIN", "PTS","xPTS", "FG","3FG","FT","TS%"]
 
 for i, (num, label) in enumerate(zip(season_stats, season_labels)):
     # Calculate x position for each pair
-    x = -130 + 50*i 
+    x = -150 + 60*i 
 
-    ax1.text(x, -70, str(num), ha='center', va='center', fontsize=8, color='black', fontweight='bold')
-    ax1.text(x, -90, label, ha='center', va='center', fontsize=5, color='grey', fontweight='medium')
+    ax1.text(x, -70, str(num), ha='center', va='center', fontsize=9, color='black', fontweight='bold')
+    ax1.text(x, -85, label, ha='center', va='center', fontsize=5, color='grey', fontweight='medium')
 
 image_ax = figure.add_axes([0.375, 0.111, 0.23, 0.23])  # [x, y, width, height]
 image_ax.imshow(player_photo)
